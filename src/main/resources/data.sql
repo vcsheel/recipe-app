2ad7f643f8b8eee8bb17fba0346665adc0554b10
vcsheel <viveksheel96@gmail.com>
2020-05-15 21:00:52 +0530

    added sql insert statements

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,12 @@
-select * from category;
+insert into category (description) values ('Italian');
+insert into category (description) values ('Indian');
+insert into category (description) values ('Continental');
+insert into category (description) values ('Fast Food');
+insert into category (description) values ('American');
+insert into category (description) values ('Chinese');
+
+insert into unit_of_measure (description) values  ('pinch');
+insert into unit_of_measure (description) values  ('tablespoon');
+insert into unit_of_measure (description) values  ('teaspoon');
+insert into unit_of_measure (description) values  ('glass');
+insert into unit_of_measure (description) values  ('cup');
